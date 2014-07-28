2b6df50ba21214197714bbf6a72a1bc3723cfcec
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-07-28 05:06:47 -0700

    Added basic Nancy+SignalR server implementation.

--- FancyChat.Portal/Program.fs
+++ FancyChat.Portal/Program.fs
@@ -1,7 +1,39 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open Owin
+open Microsoft.Owin.Hosting
+open Microsoft.Owin.Cors
+open EkonBenefits.FSharp.Dynamic
+open Nancy
+
+// Owin startup file configuring needed dependencies
+type Startup() =
+    member this.Configuration(app: Owin.IAppBuilder): unit =
+        app
+            .UseCors(CorsOptions.AllowAll)
+            .MapSignalR()
+            .UseNancy() 
+        |> ignore
+
+// SignalR hub to serve realtime requests
+type ChatHub() =
+    inherit Microsoft.AspNet.SignalR.Hub()
+
+    member this.Send(name, message): unit =
+        this.Clients.All ? addMessage(name, message)
+
+// Nancy module to serve HTTP responses
+type ChatModule() as this =
+    inherit NancyModule()
+    
+    do
+        this.Get.["/"] <- fun ctx -> box "Hello, Nancy!"
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    
+    let url = "http://localhost:8080"
+
+    use webApp = WebApp.Start<Startup> url
+    printfn "Nancy+SignalR server started on %s..." url
+    System.Console.ReadLine() |> ignore
+
+    0
