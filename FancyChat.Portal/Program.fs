d7e5dfdba414c95005c2245769d40ceeac0148bb
Tagir Magomedov <tagir.magomedov@gmail.com>
2014-07-28 08:09:54 -0700

    Minor refactoring.

--- FancyChat.Portal/Program.fs
+++ FancyChat.Portal/Program.fs
@@ -1,19 +1,17 @@
 ﻿open Owin
-open Microsoft.Owin.Hosting
-open Microsoft.Owin.Cors
+open Microsoft.Owin
 open EkonBenefits.FSharp.Dynamic
-open Nancy
 
-// Owin startup file configuring needed dependencies
+// Owin startup class for configuring needed dependencies
 type Startup() =
     member this.Configuration(app: Owin.IAppBuilder): unit =
         app
-            .UseCors(CorsOptions.AllowAll)
+            .UseCors(Cors.CorsOptions.AllowAll)
             .MapSignalR()
             .UseNancy() 
         |> ignore
 
-// SignalR hub to serve realtime requests
+// SignalR hub for realtime communication
 type ChatHub() =
     inherit Microsoft.AspNet.SignalR.Hub()
 
@@ -21,9 +19,9 @@ type ChatHub() =
     member this.Send(name: string, message: string): unit =
         this.Clients.All ? addMessage(name, message)
 
-// Nancy module to serve HTTP responses
+// Nancy module for serving HTTP requests
 type ChatModule() as this =
-    inherit NancyModule()
+    inherit Nancy.NancyModule()
     
     do
         this.Get.["/"] <- fun ctx -> box this.View.["Index"]
@@ -33,7 +31,8 @@ let main argv =
     
     let url = "http://*:8080"
 
-    use webApp = WebApp.Start<Startup> url
+    use webApp = Hosting.WebApp.Start<Startup> url
+
     printfn "Nancy+SignalR server started on %s..." url
     System.Console.ReadLine() |> ignore
 
